aa9710a362ac45c9859cd91cfd83f1e66c909b96
imufiid <imufiid98@gmail.com>
2021-01-25 08:33:24 +0700

    init project

---
+++ app/src/main/java/com/mufiid/visitblitar/ui/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.mufiid.visitblitar.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mufiid.visitblitar.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
