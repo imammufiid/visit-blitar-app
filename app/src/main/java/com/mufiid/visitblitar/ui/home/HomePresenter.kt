a7c9b74199e19bb6663a44cb26ecea5ac052cd49
imufiid <imufiid98@gmail.com>
2021-01-30 09:23:10 +0700

    add call to api data recommended

--- app/src/main/java/com/mufiid/visitblitar/ui/home/HomePresenter.kt
+++ app/src/main/java/com/mufiid/visitblitar/ui/home/HomePresenter.kt
@@ -7,15 +7,34 @@ import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.schedulers.Schedulers
 
 class HomePresenter(private val wisataView: WisataView) {
-    fun getTourism() {
+    fun getTourismRandom(random: Int? = null) {
         wisataView.loading()
         CompositeDisposable().add(
-            ApiConfig.instance().getListOfTouristAttraction()
+            ApiConfig.instance().getListOfTouristAttraction(random = random)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe({
                     when (it.status) {
                         200 -> wisataView.getDataWisata(it.message, it.data)
+                        else -> wisataView.failedGetDataWisata(it.message)
+                    }
+                    wisataView.loading(false)
+                }, {
+                    wisataView.failedGetDataWisata(it.message)
+                    wisataView.loading(false)
+                })
+        )
+    }
+
+    fun getTourismRecommended(recommended: Int? = null) {
+        wisataView.loading()
+        CompositeDisposable().add(
+            ApiConfig.instance().getListOfTouristAttraction(recommended = recommended)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    when (it.status) {
+                        200 -> wisataView.getDataWisataRecommended(it.message, it.data)
                         else -> wisataView.failedGetDataWisata(it.message)
                     }
                     wisataView.loading(false)
