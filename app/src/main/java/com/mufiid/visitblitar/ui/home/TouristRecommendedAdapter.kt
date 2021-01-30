1035fb624f1a0512e2fcdf04c585be0ad1a22769
imufiid <imufiid98@gmail.com>
2021-01-30 10:28:57 +0700

    add rounded in image view

--- app/src/main/java/com/mufiid/visitblitar/ui/home/TouristRecommendedAdapter.kt
+++ app/src/main/java/com/mufiid/visitblitar/ui/home/TouristRecommendedAdapter.kt
@@ -5,6 +5,9 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
 import com.mufiid.visitblitar.R
 import com.mufiid.visitblitar.data.source.local.entity.TourismEntity
 import com.mufiid.visitblitar.databinding.ItemTouristAttractionRecommendationBinding
@@ -26,16 +29,20 @@ class TouristRecommendedAdapter(val data: List<TourismEntity>?): RecyclerView.Ad
 
     inner class ViewHolder(private val binding: ItemTouristAttractionRecommendationBinding): RecyclerView.ViewHolder(binding.root) {
         fun bind(wisata: TourismEntity) {
+            var reqOptions = RequestOptions().apply {
+                transform(CenterCrop(), RoundedCorners(10))
+            }
             with(binding) {
                 Glide.with(itemView.context)
                     .load(wisata.image)
                     .error(R.drawable.ic_image_error)
+                    .apply(reqOptions)
                     .into(imageTourist)
                 nameTouristAttraction.text = wisata.nameTouristAttraction
                 addressTouristAttraction.text = wisata.address
                 btnDetail.setOnClickListener {
                     val intent = Intent(itemView.context, DetailActivity::class.java).apply {
-                        putExtra(AddReservationActivity.EXTRA_WISATA, wisata)
+                        putExtra(DetailActivity.EXTRAS_WISATA, wisata)
                     }
                     itemView.context.startActivity(intent)
                 }
