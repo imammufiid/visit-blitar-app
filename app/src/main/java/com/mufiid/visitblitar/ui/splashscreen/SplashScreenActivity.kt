2fb8adddbf85165b1eb9cbd153fc8b039f9acf07
imufiid <imufiid98@gmail.com>
2021-02-01 22:54:39 +0700

    created intent to main activity

--- app/src/main/java/com/mufiid/visitblitar/ui/splashscreen/SplashScreenActivity.kt
+++ app/src/main/java/com/mufiid/visitblitar/ui/splashscreen/SplashScreenActivity.kt
@@ -1,12 +1,22 @@
 package com.mufiid.visitblitar.ui.splashscreen
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import com.mufiid.visitblitar.R
+import com.mufiid.visitblitar.ui.login.LoginActivity
+import com.mufiid.visitblitar.ui.main.MainActivity
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+        supportActionBar?.hide()
+
+        Handler(mainLooper).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 2000)
     }
 }
