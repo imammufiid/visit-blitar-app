d1c5d565fc7324546d86c1e239bcdb6bad66fc61
imufiid <imufiid98@gmail.com>
2021-02-01 22:53:20 +0700

    created login feature

---
+++ app/src/main/java/com/mufiid/visitblitar/ui/login/LoginActivity.kt
@@ -0,0 +1,108 @@
+package com.mufiid.visitblitar.ui.login
+
+import android.app.ProgressDialog
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.widget.Toast
+import com.mufiid.visitblitar.R
+import com.mufiid.visitblitar.data.source.local.entity.UserEntity
+import com.mufiid.visitblitar.databinding.ActivityLoginBinding
+import com.mufiid.visitblitar.ui.main.MainActivity
+import com.mufiid.visitblitar.ui.registration.RegistrationActivity
+import com.mufiid.visitblitar.utils.pref.AuthPref
+import com.mufiid.visitblitar.utils.pref.UserPref
+import com.mufiid.visitblitar.view.LoginView
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+
+class LoginActivity : AppCompatActivity(), LoginView, View.OnClickListener {
+    private lateinit var binding: ActivityLoginBinding
+    private var presenter: LoginPresenter? = null
+    private var loading: ProgressDialog? = null
+
+    companion object {
+        const val CHECK_LOGIN = "check_login"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+        init()
+        checkLogin()
+    }
+
+    private fun checkLogin() {
+        if (AuthPref.isLoggedIn(this)) {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+    }
+
+    private fun init() {
+        CompositeDisposable().clear()
+        presenter = LoginPresenter(this)
+        loading = ProgressDialog(this)
+
+        binding.btnLogin.setOnClickListener(this)
+        binding.btnRegistration.setOnClickListener(this)
+    }
+
+    override fun successLogin(message: String?, user: UserEntity) {
+        UserPref.setUserData(this, user)
+        AuthPref.setIsLoggedIn(this, true)
+        showToast(message)
+
+        val checkLogin = intent.getBooleanExtra(CHECK_LOGIN, false)
+        if (checkLogin) {
+            finish()
+        } else {
+            Handler(mainLooper).postDelayed({
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            }, 1000)
+        }
+
+    }
+
+    override fun failedLogin(message: String?) {
+        showToast(message)
+    }
+
+    override fun loading(state: Boolean) {
+        if (state) {
+            loading?.let {
+                it.setMessage(getString(R.string.please_wait))
+                it.show()
+            }
+        } else {
+            loading?.let {
+                it.dismiss()
+            }
+        }
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.btn_login -> {
+                val username = binding.etUsername.text.toString()
+                val password = binding.etPassword.text.toString()
+                if (username == "" || password == "") {
+                    showToast("Field harus diisi")
+                } else {
+                    presenter?.login(username, password)
+                }
+            }
+            R.id.btn_registration -> {
+                startActivity(Intent(this, RegistrationActivity::class.java))
+            }
+        }
+    }
+
+    private fun showToast(message: String?) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+}
