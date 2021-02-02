c2a356edcffb14512a72cd132df980e3e90f2e89
imufiid <imufiid98@gmail.com>
2021-02-02 16:20:15 +0700

    implement room into entity

--- app/src/main/java/com/mufiid/visitblitar/data/source/local/entity/TicketEntity.kt
+++ app/src/main/java/com/mufiid/visitblitar/data/source/local/entity/TicketEntity.kt
@@ -1,54 +1,75 @@
 package com.mufiid.visitblitar.data.source.local.entity
 
 import android.os.Parcelable
+import androidx.annotation.NonNull
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
+@Entity(tableName = "ticketentities")
 data class TicketEntity(
-
-	@field:SerializedName("date")
-	val date: String? = null,
-
-	@field:SerializedName("created_at")
-	val createdAt: String? = null,
-
-	@field:SerializedName("total_visitors")
-	val totalVisitors: Int? = null,
-
-	@field:SerializedName("nik")
-	val nik: String? = null,
-
-	@field:SerializedName("updated_at")
-	val updatedAt: String? = null,
-
-	@field:SerializedName("user_id")
-	val userId: Int? = null,
-
-	@field:SerializedName("name")
-	val name: String? = null,
-
-	@field:SerializedName("tourism_id")
-	val tourismId: Int? = null,
-
-	@field:SerializedName("status_is_coming")
-	val statusIsComing: Int? = null,
-
+	@PrimaryKey
+	@NonNull
+	@ColumnInfo(name = "ticket_id")
 	@field:SerializedName("id")
 	val id: Int? = null,
 
-	@field:SerializedName("no_telp")
-	val noTelp: String? = null,
+	@ColumnInfo(name = "tourism_id")
+	@field:SerializedName("tourism_id")
+	val tourismId: Int? = null,
 
-	@field:SerializedName("email")
-	val email: String? = null,
+	@ColumnInfo(name = "user_id")
+	@field:SerializedName("user_id")
+	val userId: Int? = null,
 
+	@ColumnInfo(name = "name_tourist_attraction")
 	@field:SerializedName("name_tourist_attraction")
 	val nameTouristAttraction: String? = null,
 
+	@ColumnInfo(name = "name")
+	@field:SerializedName("name")
+	val name: String? = null,
+
+	@ColumnInfo(name = "email")
+	@field:SerializedName("email")
+	val email: String? = null,
+
+	@ColumnInfo(name = "nik")
+	@field:SerializedName("nik")
+	val nik: String? = null,
+
+	@ColumnInfo(name = "no_telp")
+	@field:SerializedName("no_telp")
+	val noTelp: String? = null,
+
+	@ColumnInfo(name = "code_reservation")
 	@field:SerializedName("code_reservation")
 	val codeReservation: String? = null,
 
+	@ColumnInfo(name = "date")
+	@field:SerializedName("date")
+	val date: String? = null,
+
+	@ColumnInfo(name = "total_visitors")
+	@field:SerializedName("total_visitors")
+	val totalVisitors: Int? = null,
+
+	@ColumnInfo(name = "status_is_coming")
+	@field:SerializedName("status_is_coming")
+	val statusIsComing: Int? = null,
+
+	@ColumnInfo(name = "created_at")
+	@field:SerializedName("created_at")
+	val createdAt: String? = null,
+
+	@ColumnInfo(name = "updated_at")
+	@field:SerializedName("updated_at")
+	val updatedAt: String? = null,
+
+	@ColumnInfo(name = "img_code_reservation")
 	@field:SerializedName("img_code_reservation")
 	val imgCodeReservation: String? = null,
 ) : Parcelable
--- app/src/main/java/com/mufiid/visitblitar/data/source/local/entity/TourismEntity.kt
+++ app/src/main/java/com/mufiid/visitblitar/data/source/local/entity/TourismEntity.kt
@@ -1,42 +1,60 @@
 package com.mufiid.visitblitar.data.source.local.entity
 
 import android.os.Parcelable
+import androidx.annotation.NonNull
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
+@Entity(tableName = "tourismentities")
 data class TourismEntity(
 
-	@field:SerializedName("image")
-	val image: String? = null,
-
-	@field:SerializedName("address")
-	val address: String? = null,
-
-	@field:SerializedName("category_id")
-	val categoryId: Int? = null,
-
-	@field:SerializedName("capasity")
-	val capasity: Int? = null,
-
-	@field:SerializedName("price_ticket")
-	val priceTicket: String? = null,
-
-	@field:SerializedName("latitude")
-	val latitude: String? = null,
-
-	@field:SerializedName("total_visitor_today")
-	val totalVisitorToday: Int? = null,
-
-	@field:SerializedName("description")
-	val description: String? = null,
-
+	@PrimaryKey
+	@NonNull
+	@ColumnInfo(name = "tourism_id")
 	@field:SerializedName("id")
 	val id: Int? = null,
 
+	@ColumnInfo(name = "category_id")
+	@field:SerializedName("category_id")
+	val categoryId: Int? = null,
+
+	@ColumnInfo(name = "name_tourist_attraction")
 	@field:SerializedName("name_tourist_attraction")
 	val nameTouristAttraction: String? = null,
 
+	@ColumnInfo(name = "address")
+	@field:SerializedName("address")
+	val address: String? = null,
+
+	@ColumnInfo(name = "capacity")
+	@field:SerializedName("capasity")
+	val capasity: Int? = null,
+
+	@ColumnInfo(name = "price_ticket")
+	@field:SerializedName("price_ticket")
+	val priceTicket: String? = null,
+
+	@ColumnInfo(name = "total_visitor_today")
+	@field:SerializedName("total_visitor_today")
+	val totalVisitorToday: Int? = null,
+
+	@ColumnInfo(name = "description")
+	@field:SerializedName("description")
+	val description: String? = null,
+
+	@ColumnInfo(name = "image")
+	@field:SerializedName("image")
+	val image: String? = null,
+
+	@ColumnInfo(name = "latitude")
+	@field:SerializedName("latitude")
+	val latitude: String? = null,
+
+	@ColumnInfo(name = "longitude")
 	@field:SerializedName("longitude")
 	val longitude: String? = null
 ) : Parcelable
