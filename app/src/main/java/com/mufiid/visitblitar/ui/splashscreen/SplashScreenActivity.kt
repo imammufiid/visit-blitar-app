4ecf60034e4e93d1c1172945205bd1ca716b75ea
imufiid <imufiid98@gmail.com>
2021-02-03 18:42:17 +0700

    add text view app version

--- app/src/main/java/com/mufiid/visitblitar/ui/splashscreen/SplashScreenActivity.kt
+++ app/src/main/java/com/mufiid/visitblitar/ui/splashscreen/SplashScreenActivity.kt
@@ -4,15 +4,22 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import com.mufiid.visitblitar.BuildConfig
 import com.mufiid.visitblitar.R
+import com.mufiid.visitblitar.databinding.ActivitySplashScreenBinding
 import com.mufiid.visitblitar.ui.login.LoginActivity
 import com.mufiid.visitblitar.ui.main.MainActivity
 
 class SplashScreenActivity : AppCompatActivity() {
+    private lateinit var binding : ActivitySplashScreenBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         supportActionBar?.hide()
+
+        binding.tvAppVersion.text = getString(R.string.app_version, BuildConfig.VERSION_NAME)
 
         Handler(mainLooper).postDelayed({
             startActivity(Intent(this, MainActivity::class.java))
