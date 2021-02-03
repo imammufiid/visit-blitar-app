23b160c54c9cd586ecfbe834ecdff5d22dee52b1
imufiid <imufiid98@gmail.com>
2021-02-03 18:44:20 +0700

    change intent to detail page

--- app/src/main/java/com/mufiid/visitblitar/ui/reservation/ReservationAdapter.kt
+++ app/src/main/java/com/mufiid/visitblitar/ui/reservation/ReservationAdapter.kt
@@ -10,6 +10,7 @@ import com.mufiid.visitblitar.R
 import com.mufiid.visitblitar.data.source.local.entity.TourismEntity
 import com.mufiid.visitblitar.databinding.ItemTouristReservationBinding
 import com.mufiid.visitblitar.ui.addreservation.AddReservationActivity
+import com.mufiid.visitblitar.ui.detail.DetailActivity
 import java.text.DecimalFormat
 import java.text.NumberFormat
 import java.util.*
@@ -44,7 +45,7 @@ class ReservationAdapter(val data: List<TourismEntity>): RecyclerView.Adapter<Re
                 capacity.text = res.getString(R.string.capasity_tourist_attraction, wisata.capasity)
                 visitors.text = res.getString(R.string.visitors_tourist_attraction, wisata.totalVisitorToday)
                 container.setOnClickListener {
-                    val intent = Intent(itemView.context, AddReservationActivity::class.java).apply {
+                    val intent = Intent(itemView.context, DetailActivity::class.java).apply {
                         putExtra(AddReservationActivity.EXTRA_WISATA, wisata)
                     }
                     itemView.context.startActivity(intent)
