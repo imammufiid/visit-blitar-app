8832eb5b42586bdc273fa86726dea041e9c8e4b1
imufiid <imufiid98@gmail.com>
2021-02-10 21:35:43 +0700

    registered reservation view model to view model factory

--- app/src/main/java/com/mufiid/visitblitar/viewmodel/ViewModelFactory.kt
+++ app/src/main/java/com/mufiid/visitblitar/viewmodel/ViewModelFactory.kt
@@ -1,4 +1,30 @@
 package com.mufiid.visitblitar.viewmodel
 
-class ViewModelFactory {
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.mufiid.visitblitar.data.TourismRepository
+import com.mufiid.visitblitar.di.Injection
+import com.mufiid.visitblitar.ui.reservation.ReservationViewModel
+
+class ViewModelFactory private constructor(private val mTourismRepository: TourismRepository) :
+    ViewModelProvider.NewInstanceFactory() {
+
+    companion object {
+        @Volatile
+        private var instance: ViewModelFactory? = null
+        fun getInstance(context: Context) : ViewModelFactory =
+            instance ?: synchronized(this) {
+                instance ?: ViewModelFactory(Injection.provideRepository(context))
+            }
+    }
+
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return when {
+            modelClass.isAssignableFrom(ReservationViewModel::class.java) -> {
+                ReservationViewModel(mTourismRepository) as T
+            }
+            else -> throw Throwable("Unknown ViewModel Class: ${modelClass.name}")
+        }
+    }
 }
