a328a3ebaf0467ac5e4d84746319df3c92e27756
imufiid <imufiid98@gmail.com>
2021-02-10 16:42:34 +0700

    add notation Dao

--- app/src/main/java/com/mufiid/visitblitar/data/source/local/room/dao/TicketDao.kt
+++ app/src/main/java/com/mufiid/visitblitar/data/source/local/room/dao/TicketDao.kt
@@ -2,11 +2,13 @@ package com.mufiid.visitblitar.data.source.local.room.dao
 
 import androidx.lifecycle.LiveData
 import androidx.paging.DataSource
+import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.mufiid.visitblitar.data.source.local.entity.TicketEntity
 
+@Dao
 interface TicketDao {
 
     @Query("SELECT * FROM ticketentities WHERE user_id = :userId")
--- app/src/main/java/com/mufiid/visitblitar/data/source/local/room/dao/TourismDao.kt
+++ app/src/main/java/com/mufiid/visitblitar/data/source/local/room/dao/TourismDao.kt
@@ -2,11 +2,13 @@ package com.mufiid.visitblitar.data.source.local.room.dao
 
 import androidx.lifecycle.LiveData
 import androidx.paging.DataSource
+import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.mufiid.visitblitar.data.source.local.entity.TourismEntity
 
+@Dao
 interface TourismDao {
 
     @Query("SELECT * FROM tourismentities")
@@ -14,6 +16,12 @@ interface TourismDao {
 
     @Query("SELECT * FROM tourismentities WHERE tourism_id = :tourismId")
     fun getTourismById(tourismId: Int?): LiveData<TourismEntity>
+
+//    @Query("SELECT * FROM tourismentities ORDER BY RAND() LIMIT 6")
+//    fun getRandomTourism(): DataSource.Factory<Int, TourismEntity>
+//
+//    @Query("SELECT * FROM tourismentities ORDER BY RAND() LIMIT 10")
+//    fun getRecommendedTourism(): DataSource.Factory<Int, TourismEntity>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertTourism(tourism: List<TourismEntity>)
