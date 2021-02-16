3a069282a3fc23815f428c60b99f868ddc48da42
imufiid <imufiid98@gmail.com>
2021-02-16 15:42:15 +0700

    init navigation controller in activity

--- app/src/main/java/com/mufiid/visitblitar/ui/main/MainActivity.kt
+++ app/src/main/java/com/mufiid/visitblitar/ui/main/MainActivity.kt
@@ -5,19 +5,15 @@ import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
 import android.os.Handler
-import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-import androidx.fragment.app.Fragment
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.mufiid.visitblitar.R
 import com.mufiid.visitblitar.databinding.ActivityMainBinding
-import com.mufiid.visitblitar.ui.home.HomeFragment
-import com.mufiid.visitblitar.ui.reservation.ReservationFragment
-import com.mufiid.visitblitar.ui.ticket.TicketFragment
 
-class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private var doubleBack = false
 
@@ -30,43 +26,12 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         checkPermission()
-        init()
+        initNavigation()
     }
 
-//    private fun checkLogin() {
-//        if(!AuthPref.isLoggedIn(this)) {
-//            finish()
-//        }
-//    }
-
-    private fun init() {
-        loadFragment(HomeFragment())
-        binding.bottomNavigationView.setOnNavigationItemSelectedListener(this)
-    }
-
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.nav_home -> {
-                loadFragment(HomeFragment())
-                return true
-            }
-            R.id.nav_my_ticket -> {
-                loadFragment(TicketFragment())
-                return true
-            }
-            R.id.nav_reservation -> {
-                loadFragment(ReservationFragment())
-                return true
-            }
-        }
-        return false
-    }
-
-    private fun loadFragment(fragment: Fragment) {
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.frame_layout, fragment, fragment.javaClass.simpleName)
-            .commit()
+    private fun initNavigation() {
+        val navController = findNavController(R.id.nav_host_fragment)
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
 
     override fun onBackPressed() {
