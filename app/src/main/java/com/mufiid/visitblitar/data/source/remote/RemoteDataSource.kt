4183384da8f72cd462237b893b20683d0665632d
imufiid <imufiid98@gmail.com>
2021-02-16 16:54:34 +0700

    create function to search tourism from remote

--- app/src/main/java/com/mufiid/visitblitar/data/source/remote/RemoteDataSource.kt
+++ app/src/main/java/com/mufiid/visitblitar/data/source/remote/RemoteDataSource.kt
@@ -4,7 +4,6 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.mufiid.visitblitar.api.ApiConfig
 import com.mufiid.visitblitar.data.source.local.entity.TourismEntity
-import com.mufiid.visitblitar.data.source.remote.response.WrappedListResponses
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.schedulers.Schedulers
@@ -38,4 +37,25 @@ class RemoteDataSource {
         )
         return result
     }
+
+    fun searchTourism(query: String?): LiveData<ApiResponse<List<TourismEntity>>> {
+        val result = MutableLiveData<ApiResponse<List<TourismEntity>>>()
+        CompositeDisposable().add(
+            ApiConfig.instance().getListOfTouristAttraction(query = query)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    when (it.status) {
+                        200 -> result.value = ApiResponse.success(it.data)
+                        404 -> result.value = ApiResponse.empty(it.message)
+                        else -> result.value = ApiResponse.failed(it.message)
+                    }
+                }, {
+                    result.value = ApiResponse.error(it.message)
+                })
+        )
+        return result
+    }
+
+    suspend fun getAllMyTicket(userId: Int) = ApiConfig.instance().getMyTicket(userId)
 }
