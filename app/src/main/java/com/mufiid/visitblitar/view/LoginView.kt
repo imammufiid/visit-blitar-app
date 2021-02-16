e076a5cc49fde095aa817e8c8c5252a102c9de50
imufiid <imufiid98@gmail.com>
2021-02-16 08:15:45 +0700

    deleted view component MVP

--- app/src/main/java/com/mufiid/visitblitar/view/LoginView.kt
+++
@@ -1,8 +0,0 @@
-package com.mufiid.visitblitar.view
-
-import com.mufiid.visitblitar.data.source.local.entity.UserEntity
-
-interface LoginView: LoadingView {
-    fun successLogin(message: String?, user: UserEntity)
-    fun failedLogin(message: String?)
-}
--- app/src/main/java/com/mufiid/visitblitar/view/ReservationView.kt
+++
@@ -1,8 +0,0 @@
-package com.mufiid.visitblitar.view
-
-import com.mufiid.visitblitar.data.source.local.entity.TourismEntity
-
-interface ReservationView: LoadingView {
-    fun getDataReservation(message: String?, data: List<TourismEntity>)
-    fun failedGetDataReservation(message: String?)
-}
